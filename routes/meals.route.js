e1c864e140c5873e644cb0b049bad4247d383775
Vincent Iroleh <irolehiroleh@gmail.com>
2019-02-16 21:49:45 +0100

    chore: setup express router and definds our routes

---
+++ routes/meals.route.js
@@ -0,0 +1,32 @@
+// imported express and our dummy database
+import express from 'express';
+
+// Import mealsController
+import mealController from '../controllers/meals.controllers';
+
+// Initialized express router
+const router = express.Router();
+
+
+// TODO: OUR ENDPOINTS
+
+// get all meals
+router.get("/api/v1/meals", mealController.getAllMeals);
+
+// Get a single meal
+router.get('/api/v1/meals/:id', mealController.getMeal);
+
+// post a meal
+router.post("/api/v1/meals", mealController.createMeal);
+
+// Update a meal
+router.put('/api/v1/meals/:id', mealController.updateMeal);
+
+// Delete a single meal
+router.delete('/api/v1/meals/:id', mealController.deleteMeal);
+
+
+// TODO: Endpoints ends here
+
+// Export router
+export default router;
